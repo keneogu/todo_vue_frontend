23aa5083286025d996757157e3776cfa538f4b7e
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-10-28 21:19:03 +0100

    Add header

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="w-full bg-orange-800 hover:bg-orange-600 h-10 flex justify-center items-center">
+    <h1 class="text-white text-xl font-bold decoration-dotted underline p-2">{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    title: String,
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="flex justify-center items-center flex-col">
+	<div :key="todo.id" v-for="todo in todos" class="mt-4 w-1/2 flex-col">
+		<Todo :todo="todo" @toggle-complete="$emit('toggle-complete', todo.id)" @delete-todo="$emit('delete-todo', todo.id)"/>
+	</div>
+</div>
+</template>
+
+<script>
+
+import Todo from './Todo.vue'
+
+export default {
+	name: 'Tasks',
+	props: {
+		todos: Array,
+	},
+	components: {
+		Todo,
+	},
+	emits: ['delete-todo', 'toggle-complete']
+}
+</script>
+
+<style>
+
+</style>
