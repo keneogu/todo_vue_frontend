384b7dd3a5bf6713176a9629e0a6727ca1c52cf2
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-11-04 12:38:22 +0100

    Add modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="backdrop" @click="closeModal">
+		<div class="modal" :class="{ sale: theme === 'sale'}">
+			<slot></slot>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ['theme'],
+	methods: {
+		closeModal() {
+			this.$emit('close')
+		}
+	}
+};
+</script>
+
+<style>
+	.modal {
+		width: 400px;
+		padding: 100px 20px;
+		margin: 100px auto;
+		background: white;
+		border-radius: 10px;
+	}
+	.backdrop {
+		top: 0;
+		position: fixed;
+		background: rgba(0, 0, 0, 0.5);
+		width: 100%;
+		height: 100%;
+	}
+	.modal h1 {
+		color: #03cfb4;
+		border: none;
+		padding: 0;
+	}
+	.modal p {
+		 font-style: normal;
+	}
+	.modal.sale {
+		background: #9a3412;
+		color: white;
+	}
+</style>
