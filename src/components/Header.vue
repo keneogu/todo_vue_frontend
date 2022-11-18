fa0bcd3362fa38869facddc64f70c5b351ed3f6d
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-11-18 09:57:02 +0100

    Add logout button to header section

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
-  <header class="w-full bg-orange-800 hover:bg-orange-600 h-10 flex justify-center items-center">
+  <header class="w-full bg-orange-800 hover:bg-orange-600 h-10 flex justify-between items-center">
     <h1 class="text-white text-4xl md:text-xl font-bold decoration-dotted underline p-2">{{ title }}</h1>
+    <button @click="$emit('logout')" class="text-white">logout</button>
   </header>
 </template>
 
@@ -9,7 +10,7 @@ export default {
   name: 'Header',
   props: {
     title: String,
-  },
+  }
 };
 </script>
 
